db62679312193857a969919135ecdb05220fa51f
ismapro22 <eslove1014@naver.com>
2024-11-10 23:27:39 +0900

    not working yet
    아직 백엔드랑 연결 작업 중. 프로젝트 새로 파야 할 수도 있음

--- src/App.js
+++ src/App.js
@@ -2,9 +2,18 @@
 
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+// import { useState } from 'react';
 
 function App() { //실은 유우명한 컴포넌트임
+
+  const [message, setMessage] = useState("")
+ 
+  useEffect(()=>{
+    fetch("/test")
+      .then(res => res.text())
+      .then(m=>setMessage(m))
+  }, [])
 
   let post = '학습용 제목 영역';//코드 중간에 바뀌어도 바로 반영X
   let [글제목1, b1] = useState('가을엔 전어구이');//스테이트는 반영 바로 됨
@@ -17,7 +26,7 @@ function App() { //실은 유우명한 컴포넌트임
   //let num = [1, 2];
   //let [a, c] = [1, 2];//디스트럭쳐링 문법
   //let a = num[0];  //let c = num[1];
-
+ 
   function 함수(){
     console.log(1);
     //state는 등호로 변경하면 반영이 안 됨
@@ -34,53 +43,55 @@ function App() { //실은 유우명한 컴포넌트임
         //2. 변수 넣을 땐 {변수명}
         //3. style 넣을 땐 style={{이름:'값'}}
       }            
+      <header className="App-header">
+        <div className="black-nav">
+          <h4 style={ {color : 'red', fontSize : '20px'} }>{logo}</h4>
+          {/*html은 <h4 style={"color : red"}>지만 JSX는 위와 같음*/}
+          {/*<h4 id={post}> 이렇게도 가능*/}
+        </div>
 
-      <div className="black-nav">
-        <h4 style={ {color : 'red', fontSize : '20px'} }>{logo}</h4>
-        {/*html은 <h4 style={"color : red"}>지만 JSX는 위와 같음*/}
-        {/*<h4 id={post}> 이렇게도 가능*/}
-      </div>
+        <button onClick={()=>{
+            let copy2 = [...글제목];
+            copy2.sort();
+            b(copy2);
+          }}> 가나다순 정렬</button>
 
-      <button onClick={()=>{
-          let copy2 = [...글제목];
-          copy2.sort();
-          b(copy2);
-        }}> 가나다순 정렬</button>
+        <button onClick={()=>{
+          let arr = [1,2,3];//array/object 담은 변수엔 위치만 저장됨
+          
 
-      <button onClick={()=>{
-        let arr = [1,2,3];//array/object 담은 변수엔 위치만 저장됨
+          //array/object 요약
+          //state가 array/object면 독립적 카피본(shallow copy)을 만들어서 수정해야 함
+
+          let copy = 글제목1;//[...글제목] <-array는 이렇게(화살표 바꿔달라는 뜻)
+          //state 변경함수 특 기존이랑 신규state가 같으면 변경 안 해줌
+          copy = '봄에는 유채꽃이지';
+          b1(copy)}}> 하지만 봄에는? </button>
+
+        <div className="list">
+          <h4>{글제목1} <span onClick={()=>{changeLike(likes+1)}/**이 안에 함수채로 만들어도 됨 */}>❤</span> {likes} </h4>
+          <p>11월 07일 발행</p> 
+        </div>
         
 
-        //array/object 요약
-        //state가 array/object면 독립적 카피본(shallow copy)을 만들어서 수정해야 함
+        <div className="list">
+          <h4>{글제목[0]}</h4>
+          <p>11월 06일 발행</p>
+        </div>
+        
+        <div className="list">
+          <h4>{글제목[1]}</h4>
+          <p>11월 05일 발행</p>
+        </div>
 
-        let copy = 글제목1;//[...글제목] <-array는 이렇게(화살표 바꿔달라는 뜻)
-        //state 변경함수 특 기존이랑 신규state가 같으면 변경 안 해줌
-        copy = '봄에는 유채꽃이지';
-        b1(copy)}}> 하지만 봄에는? </button>
+        <h4>{ post }</h4>{
+          //중괄호{}로 변수를 넣는다. 데이터 바인딩
+        }
 
-      <div className="list">
-        <h4>{글제목1} <span onClick={()=>{changeLike(likes+1)}/**이 안에 함수채로 만들어도 됨 */}>❤</span> {likes} </h4>
-        <p>11월 07일 발행</p> 
-      </div>
-      
-
-      <div className="list">
-        <h4>{글제목[0]}</h4>
-        <p>11월 06일 발행</p>
-      </div>
-      
-      <div className="list">
-        <h4>{글제목[1]}</h4>
-        <p>11월 05일 발행</p>
-      </div>
-
-      <h4>{ post }</h4>{
-        //중괄호{}로 변수를 넣는다. 데이터 바인딩
-      }
-
-      {/* <Modal></Modal> 이렇게 쓰거나 아래처럼 바로 닫아도 됨 */}
-      <Modal/>
+        {/* <Modal></Modal> 이렇게 쓰거나 아래처럼 바로 닫아도 됨 */}
+        <Modal/>
+        
+      </header>
       
 
     </div>
